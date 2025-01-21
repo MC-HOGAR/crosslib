3177994719d1dce98a2f59e6b8f8e853259b6d7b
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-01-21 12:22:36 -0300

    fix type. el username se pasa en el path y no en el body.

--- src/modules/Usuarios/InternalUser/types/internalUserHttp.types.ts
+++ src/modules/Usuarios/InternalUser/types/internalUserHttp.types.ts
@@ -2,7 +2,7 @@ import { IInternalUser } from "./InternalUser.types"
 
 export type GetAllInternalUsersResData = IInternalUser[]
 
-export type UpdateInternalUserReqPayload = Pick<IInternalUser, "id" | "group" | "status">
+export type UpdateInternalUserReqPayload = Pick<IInternalUser, "group" | "status">
 
 export interface UpdateInternalUserMetadataQueryStringParameters {
     lastLogin?: "true"
