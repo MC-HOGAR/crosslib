560a2e49e05c9b59cc61136d7743af7b0c950f2c
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-03-13 09:34:16 -0300

    agrego funcion para aplicar reglas de negocio.

---
+++ src/modules/Articulos/schemas/ArticuloPrecio.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+import { getToday } from '@/common/utils/DateUtils'
+
+const today = getToday()
+
+export const ArticuloPrecioZodSchema = z.object({
+    arp_utilidad_web: z.number().min(0, 'Utilidad Web: debe ser mayor o igual a 0').max(200, 'Utilidad Web: debe ser menor o igual a 200'),
+    arp_utilidad_ofer: z.number().min(0, 'Utilidad Oferta: debe ser mayor o igual a 0').max(200, 'Utilidad Oferta: debe ser menor o igual a 200').nullish(),
+    arp_utilidad_ofer_fecha_hasta: z.date().refine(date => date >= today, { message: 'Utilidad Oferta Fecha Hasta: la fecha debe ser igual o mayor a hoy' }).nullish(),
+    arp_utilidad_ofer_stock_hasta: z.number().min(0, 'Utilidad Oferta Stock Hasta: debe ser mayor o igual a 0').nullish(),
+    arp_descuento: z.number().min(0, 'Descuento: debe ser mayor o igual a 0').max(200, 'Descuento: debe ser menor o igual a 200').nullish(),
+    arp_descuento_fecha_hasta: z.date().refine(date => date >= today, { message: 'Descuento Fecha Hasta: la fecha debe ser igual o mayor a hoy' }).nullish(),
+  })
--- src/modules/Articulos/utils/ArticuloPrecio.utils.ts
+++ src/modules/Articulos/utils/ArticuloPrecio.utils.ts
@@ -20,8 +20,38 @@ function calcularDescuento (precioBase: Decimal, porcentajeDescuento: Decimal) {
     return precioBase.sub(descuento); // Aplicar descuento al precio base
 }
 
+interface ReglaNegocioInput {
+    arp_utilidad_web: number;
+    arp_utilidad_ofer: number | null;
+    arp_utilidad_ofer_fecha_hasta: string | null;
+    arp_utilidad_ofer_stock_hasta: number | null;
+    arp_descuento: number | null;
+    arp_descuento_fecha_hasta: string | null
+  }
+
+function aplicarReglasDeNegocio ({
+    arp_utilidad_web,
+    arp_utilidad_ofer,
+    arp_utilidad_ofer_fecha_hasta,
+    arp_utilidad_ofer_stock_hasta,
+    arp_descuento,
+    arp_descuento_fecha_hasta
+}: ReglaNegocioInput) {
+    const errores: string[] = [];
+    if (!arp_utilidad_ofer && (arp_utilidad_ofer_fecha_hasta || typeof(arp_utilidad_ofer_stock_hasta) === 'number')) {
+        errores.push('Utilidad Oferta: Se debe aplicar un valor si se define una condición')
+    }
+    if(arp_utilidad_ofer_fecha_hasta && typeof(arp_utilidad_ofer_stock_hasta) === 'number') {
+        errores.push('Se debe usar únicamente 1 condición para la Utilidad Oferta (Usando Fecha Hasta y Stock Hasta)')
+    }
+    if (!arp_descuento && arp_descuento_fecha_hasta) {
+        errores.push('Descuento: Se debe aplicar un valor si se aplica una condición')
+    }
+}
+
 export {
     calcularPrecio,
     calcularPorcentajeOff,
-    calcularDescuento
+    calcularDescuento,
+    aplicarReglasDeNegocio
 }
