3390a60733d98eb01cb55c1498f21f7880e70a81
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-05-28 11:05:45 -0300

    bump

--- src/modules/HomeCMS/BannerCarousel/types/BannerCarousel.types.ts
+++ src/modules/HomeCMS/BannerCarousel/types/BannerCarousel.types.ts
@@ -10,3 +10,14 @@ export interface BannerCarouselDocData {
 export interface BannerCarousel extends BannerCarouselDocData {
     id: string
 }
+
+export const BannerCarouselFileValidations = Object.freeze({
+    MAX_FILE_SIZE: 7 * 1024 * 1024, // 7MB
+    IMG_MOBILE_WIDTH: 500,
+    IMG_MOBILE_HEIGHT: 340,
+    IMG_MOBILE_RESOLUTION: '500x340',
+    IMG_DESKTOP_WIDTH: 1920,
+    IMG_DESKTOP_HEIGHT: 436,
+    IMG_DESKTOP_RESOLUTION: '1920x436',
+    VALID_IMAGE_TYPES: ['jpg','jpeg', 'png', 'webp', 'avif']
+})
--- src/modules/HomeCMS/Esferas/types/Esferas.types.ts
+++ src/modules/HomeCMS/Esferas/types/Esferas.types.ts
@@ -7,3 +7,11 @@ export interface Esfera {
 export interface EsferaConId extends Esfera {
     id: string
 }
+
+export const EsferasFileValidation = Object.freeze({
+    MAX_FILE_SIZE: 7 * 1024 * 1024, // 7MB
+    MAX_WIDTH: 500,
+    MAX_HEIGHT: 500,
+    IMG_RESOLUTION: '500x500',
+    VALID_IMAGE_TYPES: ['jpg','jpeg', 'png', 'webp', 'avif']
+})
