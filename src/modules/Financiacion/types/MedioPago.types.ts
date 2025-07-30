b5951a072ef348d606fc4524e595a1b4bdac95f8
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-07-30 12:30:05 -0300

    up

---
+++ src/modules/Financiacion/types/MedioPago.types.ts
@@ -0,0 +1,7 @@
+export interface MedioPago {
+    id: number
+    nombre: string
+    orden: number
+    logoUrl: string | null
+    comentarios: string | null
+}
---
+++ src/modules/Financiacion/types/index.ts
@@ -0,0 +1,1 @@
+export * from './MedioPago.types'
