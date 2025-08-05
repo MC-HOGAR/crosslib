ad6d8ef72da2475a415f16b0714d6bb006f563fd
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-08-05 10:57:31 -0300

    up

--- src/modules/Financiacion/types/MedioPago.types.ts
+++ src/modules/Financiacion/types/MedioPago.types.ts
@@ -1,20 +1,20 @@
 export interface MedioPago {
     id: number
+    codigo_interno: string | null
     nro_terminal: string | null
     tipo_medio: TipoMedioPago
-    codigo_medio: string | null
+    es_digital: boolean
     nombre: string
-    orden: number
+    orden: number | null
     logoUrl: string | null
     comentarios: string | null
     activo: boolean
     disponible_online: boolean
-    es_digital: boolean
     created_at: string
     updated_at: string
 }
 
-enum TipoMedioPago {
+export enum TipoMedioPago {
   TARJETA = "TARJETA",                                  // Visa, MasterCard, Naranja, etc.
   TRANSFERENCIA_EFECTIVO = "TRANSFERENCIA_EFECTIVO",    // CBU, alias
   CREDITO_PERSONAL = "CREDITO_PERSONAL"                 // Financiamiento de la empresa
--- src/modules/Financiacion/types/Tarjetas.types.ts
+++ src/modules/Financiacion/types/Tarjetas.types.ts
@@ -1,9 +1,17 @@
 export interface Tarjeta {
     id: number
     nombre: string
+    codigo_tarjeta: string | null
+    tipo_tarjeta: TipoTarjeta
     orden: number
     logoUrl: string | null
     comentarios: string | null
-    esDebito: boolean
     activo: boolean
+    created_at: string
+    updated_at: string
+}
+
+export enum TipoTarjeta {
+  CREDITO = "CREDITO",
+  DEBITO = "DEBITO",
 }
