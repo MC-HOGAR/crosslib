914a428a5fe14f48bef1b2ce28009c950aca88cd
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-08-21 19:51:38 -0300

    up

--- src/modules/Financiacion/types/NroComercio.types.ts
+++ src/modules/Financiacion/types/NroComercio.types.ts
@@ -1,3 +1,13 @@
+export interface NroComercio {
+    id: number;
+    comentarios: string | null;
+    servicio_pago_id: number;
+    nro_comercio: string;
+    banco_id: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
 export interface SelectEntidad {
     id: number;
     nombre: string;
