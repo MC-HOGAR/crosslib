fb0cf974850d576f29c5250c702c6f438e693e47
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-10-17 12:29:15 -0300

    agregamos types para el nuevo modulo de etiquetas.

---
+++ src/modules/Etiquetas/EtiquetasGenerales/types/ArticuloEtiquetaGeneral.types.ts
@@ -0,0 +1,6 @@
+export interface ArticuloEtiquetaGeneral {
+    created_at: string;
+    aik_ar_codigo: string;
+    etiqueta_id: number;
+    orden: number;
+}
---
+++ src/modules/Etiquetas/EtiquetasGenerales/types/EtiquetaGeneral.types.ts
@@ -0,0 +1,19 @@
+export interface EtiquetaGeneral {
+    id: number;
+    nombre: string;
+    nombreWeb: string;
+    slug: string;
+    comentarios: string | null;
+    imagen_url: string | null;
+    activo: boolean;
+    fecha_desde_valido: string | null;
+    fecha_hasta_valido: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
+export type EtiquetaGeneralCrearDto = Pick<EtiquetaGeneral, 'nombre' | 'nombreWeb' | 'slug' | 'comentarios' | 'fecha_desde_valido' | 'fecha_hasta_valido'>
+
+export type EtiquetaGeneralActualizarDto = Pick<EtiquetaGeneral, 'nombre' | 'nombreWeb' | 'comentarios' | 'activo' | 'fecha_desde_valido' | 'fecha_hasta_valido'>
+
+export type EtiquetaGeneralCrearActualizarFormState = Pick<EtiquetaGeneral, 'nombre' | 'nombreWeb' | 'slug' | 'comentarios' | 'activo' | 'fecha_desde_valido' | 'fecha_hasta_valido'>
