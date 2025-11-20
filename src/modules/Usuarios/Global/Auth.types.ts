5b8ef9f96b3893846119c7dea4f27f55048dfa92
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-11-20 11:23:46 -0300

    up

--- src/modules/Usuarios/Global/Auth.types.ts
+++ src/modules/Usuarios/Global/Auth.types.ts
@@ -1,7 +1,8 @@
-/* Shared types for modules InternalUser and CustomerUser */
-export type UserType = 'Internal' | 'Customer'
+/* Shared types for modules InternalUser, CustomerUser, SalesUser */
+export type UserType = 'Internal' | 'Customer' | 'Sales'
 
 export enum UserTypeEnum {
     Internal = 'Internal',
-    Customer = 'Customer'
+    Customer = 'Customer',
+    Sales = 'Sales',
 }
