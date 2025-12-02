d3ed10aedf16df09140f9f6d46d76a8f48c1d34f
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-12-02 18:24:35 -0300

    up

--- src/modules/Usuarios/SalesUser/utils/SalesUser.utils.ts
+++ src/modules/Usuarios/SalesUser/utils/SalesUser.utils.ts
@@ -33,3 +33,38 @@ export function getSalesUserStatusLabel(status: SalesUserStatusEnum): string {
   const option = SalesUserStatusOptions.find(opt => opt.value === status);
   return option?.label || status;
 }
+
+export const VALIDATION_RULES = Object.freeze({
+  USERNAME: {
+    MIN_LENGTH: 3,
+    MAX_LENGTH: 25,
+    PATTERN: /^[a-zA-Z0-9._-]+$/
+  },
+  FULLNAME: {
+    MIN_LENGTH: 3,
+    MAX_LENGTH: 30,
+    PATTERN: /^[a-zA-Z\sñÑáéíóúÁÉÍÓÚ]*$/
+  },
+  DNI: {
+    MIN_LENGTH: 4,
+    MAX_LENGTH: 10,
+    PATTERN: /^[0-9]*$/
+  },
+  TELEFONO: {
+    MAX_LENGTH: 50,
+    PATTERN: /^\d+-\d+$/      // Este pattern es para el campo completo. Ejem: "3492-435047" o "3493-498565".
+  },
+  AIK_VEN_CODIGO: {
+    MAX_LENGTH: 20,
+    PATTERN: /^[0-9]*$/
+  },
+  AIK_DE_CODIGO: {
+    MAX_LENGTH: 10,
+    PATTERN: /^[0-9]*$/
+  },
+  AIK_ZO_CODIGO: {
+    MAX_LENGTH: 10,
+    PATTERN: /^[0-9]*$/
+  }
+})
+
